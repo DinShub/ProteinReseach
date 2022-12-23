3ab8f1111725171161938a269c1e0057b1a91170
Din Shub <din.shub199@gmail.com>
2022-12-23 18:30:42 +0200

    created jsonl from fasta

--- create-dataset.py
+++ create-dataset.py
@@ -1,4 +1,25 @@
 from Bio import SeqIO
+import pandas as pd
 
-for record in SeqIO.parse('FASTA_any.fa', 'fasta'):
-  print(record.seq)
+# Read all the records from fasta
+records_any = SeqIO.parse('FASTA_any.fa', 'fasta')
+records_human = SeqIO.parse('FASTA_human.fa', 'fasta')
+records_swine = SeqIO.parse('FASTA_swine.fa', 'fasta')
+
+# dict = [{label: 'Human', sequence: 'dasfdsdafg'}, ...]
+results = {'label': [], "sequence": [], "length": []}
+for record in records_any:
+  results['label'].append(2)
+  results['sequence'].append(str(record.seq))
+  results['length'].append(len(record.seq))
+for record in records_human:
+  results['label'].append(1)
+  results['sequence'].append(str(record.seq))
+  results['length'].append(len(record.seq))
+for record in records_swine:
+  results['label'].append(0)
+  results['sequence'].append(str(record.seq))
+  results['length'].append(len(record.seq))
+
+df = pd.DataFrame(results)
+df.to_json('./all_records.jsonl', orient='records', lines=True)
